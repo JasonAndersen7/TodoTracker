4d9e4c74724251e4bdf5cc41d42efe58f4f0a20e
JasonAndersen7 <andersen.jason7@gmail.com>
2017-10-06 19:46:46 -0600

    Add files via upload
    Initial Check in of Data Layer and Changes

---
+++ Models/Todo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoTrackerModels
+
+{
+    /// <summary>
+    /// Class that contains a single todo item
+    /// </summary>
+    public class Todo
+    {
+        public int TodoID { get; set; }
+        public string Requester { get; set; }
+        public string Assignee { get; set; }
+        public DateTime DueDate { get; set; }
+        public int IsCompleted { get; set; }
+
+
+    }
+}
